b0c61a1e99d504efe677f6006d813930f638c60c
S-MW <72529306+S-MW@users.noreply.github.com>
2021-12-04 14:02:12 +0300

    add login page with functionality

--- src/component/SignIn.js
+++ src/component/SignIn.js
@@ -1,5 +1,6 @@
 // import {useDispatch, useSelector} from "react-redux"
-// import { useState } from "react"
+import { useState } from "react"
+import axios from "axios";
 // import { useNavigate } from 'react-router-dom';
 // import {logIn} from "../reducers/users/actions"
 import "./SignIn.css"
@@ -16,18 +17,75 @@ function SignIn() {
   //     userIsLogedIn: state.usersReducer
   //   }
   // });
-  // const[userMail, setUserMail] = useState("")
-  // const[password,setpassword] = useState("")
+  const[email, setEmail] = useState("")
+  const[password,setPassword] = useState("")
 
   // it will save the user email in the useState
-  // const getUserEmail = (e) => {
-  //   setUserMail(e.target.value)
-  // }
+  const getUserEmail = (e) => {
+    setEmail(e.target.value)
+  }
 
   // it will save the user password in the useState
-  // const getPassword= (e) => {
-  //     setpassword(e.target.value)
-  // }
+  const getPassword= (e) => {
+    setPassword(e.target.value)
+  }
+  
+  // [Saad]: GET request via Axios (email&pass)
+  const loginClick = () => {
+    let userInfo = {email: email, password: password}
+
+    // ---------------------3-------------------
+      console.log('login clicked')
+      let data = JSON.stringify({
+        email: email,
+        password: password
+      })
+      axios.post('http://localhost:8080/users/login', data, {
+          headers: {
+              'Content-Type': 'application/json',
+          }
+      }
+      )
+      .then(
+        function(res)
+        {
+          console.log(res)
+        }
+        )
+      .catch(function(err){console.log(err.response.data)})
+  
+
+    // --------------------2 -------------------
+    // axios({
+    //   method: 'get',
+    //   url:'http://localhost:8080/users/login',
+    //   headers: {}, 
+    //   data: 
+    //     {
+    //       email:"s@s.com",
+    //       password:"123"
+    //   }
+      
+    // })
+    // .then(
+    //   function(res)
+    //   {
+    //     console.log(res)
+    //   }
+    //   )
+    // .catch(function(err){console.log(err)})
+
+    // ----------------------------1 ---------------
+    // axios.get('http://localhost:8080/users')
+    // .then(
+    //   function(res)
+    //   {
+    //     console.log(res)
+    //   }
+    //   )
+    // .catch(function(err){console.log(err)})
+    // navigate("/Admin")
+  } 
 
   // after clicking the Login button the dispatch will send the user info (email,password) to the reducer to check it
   // const loginClick = () => {
@@ -46,11 +104,11 @@ function SignIn() {
             <div className="sign-in-div">
             <h1 id="titleid">Sign In</h1>
             <hr/>
-              <input  type="email" id="email" name="email" placeholder="Email"/>  
+              <input onChange={getUserEmail} type="email" id="email" name="email" placeholder="Email"/>  
               <br/>
-              <input  type="password" id="password" name="password" placeholder="Password"/>
+              <input onChange={getPassword} type="password" id="password" name="password" placeholder="Password"/>
               <br/>
-              <button  type="button" className="btn btn-success" id="btnColor">Login</button>
+              <button  onClick={loginClick} type="button" className="btn btn-success" id="btnColor">Login</button>
             </div>
       </div>
     );
